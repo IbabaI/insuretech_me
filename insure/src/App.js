ace7093fa5b6c41ad9591c8f0fbbc00d3e0c26a1
707BIGBOSS\user <2001mist@naver.com>
2022-03-23 16:12:40 +0900

    React-router-dom 라이브러리 추가
    App.js
    라우팅 컴포넌트 작성
    
    Merge branch 'master' of https://github.com/skymill2000/insuretech_220321

--- insure/src/App.js
+++ insure/src/App.js
@@ -1,15 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
-import Header from './components/Header';
-import InputTest from './components/InputTest';
-import ListComponent from './components/ListComponent';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import InputTest from "./components/InputTest";
+import ListComponent from "./components/ListComponent";
 
 function App() {
   return (
-    <>
-      <InputTest></InputTest>
-      <ListComponent></ListComponent>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/test" element={<InputTest />}></Route>
+        <Route path="/test2" element={<ListComponent />}></Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
