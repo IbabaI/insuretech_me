730b701ec27a17ed12e594ed67385b21f61824b5
707BIGBOSS\user <2001mist@naver.com>
2022-03-23 11:18:27 +0900

    배열 선언 / 사용 실습

---
+++ pracJavascript/prac04.js
@@ -0,0 +1,6 @@
+const array = [1, "test", {name : "name"}, 123.42312];
+console.log(array[2])
+
+// in java -> java는 형태가 같아야 한다. 
+// public String [] StringArray = new String [5];
+// public ArrayList(String) arrayListString = new ArrayList(String);
