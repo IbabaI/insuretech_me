5ca04ada56def89f21211f7db51a570d17ada967
707BIGBOSS\user <2001mist@naver.com>
2022-03-23 10:36:19 +0900

    자바스크립트 변수 실습

---
+++ pracJavascript/prac01.js
@@ -0,0 +1,31 @@
+var a = "hello";
+var a = "bay";
+
+let b = "test";
+let c = "test2";
+b = "test3";
+
+const d =" test";
+let name1 = "name";
+let age = 15;
+let height = 144.32;
+
+const method = () => {
+    console.log("hello");
+};
+
+const array = {a, b, c};
+
+const obj = {
+    name: "test",
+    age: 14,
+};
+
+console.log(name1, age, height)
+console.log(a);
+console.log(b);
+
+// java 는 타입을 지정해야한다.
+// private String name = "name";
+// String 이라는 타입을 지정
+// private int age = 15;
