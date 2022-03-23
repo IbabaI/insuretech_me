8627788691a1dc438ef7c24298e9723c1ec809f4
707BIGBOSS\user <2001mist@naver.com>
2022-03-23 13:24:35 +0900

    옛날 웹 페이지 구성방식 템플릿 리터럴

--- pracJavascript/work04.js
+++ pracJavascript/work04.js
@@ -40,7 +40,8 @@ const cars = [car, car1, car2]
 // }
 for (car of cars){
     if(car.carname === "bmw"){
-        console.log("해당차량의 마력은 " + car.ph +"입니다");
+        //console.log("해당차량의 마력은 " + car.ph +"입니다");
+        console.log(`${car.carname}차량의 마력은 ${car.ph} 입니다`);
     }
 }
 
