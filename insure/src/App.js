2eed60a71bc1cd73bf16bb898b86fc7b8bf44b0c
707BIGBOSS\user <2001mist@naver.com>
2022-03-23 14:09:45 +0900

    컴포넌트 프롭스 기초

--- insure/src/App.js
+++ insure/src/App.js
@@ -1,8 +1,9 @@
 import logo from './logo.svg';
 import './App.css';
 
-const Welcome = () => {
-  return <h2>저는 새로 추가된 컴포넌트 입니다.</h2>;
+const Welcome = ({userName, age}) => {
+  //console.log(props);
+  return <h2>안녕하세요{userName} {age}</h2>;
 };
 
 function App() {
@@ -10,12 +11,10 @@ function App() {
     <>
       <h1>안녕하세요</h1>
       <h3>리엑트 입니다</h3>
-      <Welcome></Welcome>
+      <Welcome userName={"홍길동"} age={45}></Welcome>
+      <Welcome userName={"고귀남"} age={13}></Welcome>
     </>
-
   );
- 
-
 }
 
 export default App;
