9c5e916319fa8749385b0adee2ae7e44912cb9dc
707BIGBOSS\user <2001mist@naver.com>
2022-03-24 11:32:50 +0900

    뉴스 검색 페이지 완성

--- insure/src/App.js
+++ insure/src/App.js
@@ -3,15 +3,17 @@ import Header from "./components/Header";
 import InputTest from "./components/InputTest";
 import ListComponent from "./components/ListComponent";
 import AxiosTest from "./pages/AxiosTest";
+import NewSearchPage from "./pages/NewSearchPage";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-      <Route path="/" element={<Header />}></Route>
-        <Route path="/test1" element={<InputTest />}></Route>
+        <Route path="/" element={<Header />}></Route>
+        <Route path="/test" element={<InputTest />}></Route>
         <Route path="/test2" element={<ListComponent />}></Route>
         <Route path="/axios" element={<AxiosTest />}></Route>
+        <Route path="/news" element={<NewSearchPage />}></Route>
       </Routes>
     </BrowserRouter>
   );
--- insure/src/pages/AxiosTest.js
+++ insure/src/pages/AxiosTest.js
@@ -4,19 +4,23 @@ import Header from "../components/Header";
 
 const AxiosTest = () => {
   const handleClick = () => {
-    axios.get(
-        "https://newsapi.org/v2/everything?q=tesla&from=2022-02-23&sortBy=publishedAt&apiKey=84c96b195b1e4e97b8cb2e5cd8977e9f"
-        ).then(function (response) {
-      // handle success
-      console.log(response);
-    });
+    let data = "데이터 받기전";
+    axios
+      .get(
+        `https://newsapi.org/v2/everything?q=tesla&from=2022-02-25&sortBy=publishedAt&apiKey=78bc6ddd8cdb48ceac76f5f9b9dfc4c5`
+      )
+      .then(function (response) {
+        // handle success
+        data = response;
+        console.log(data);
+      });
   };
-  return(
-    <>  
-        <Header title={"데이터 요청 실습"}></Header>
-        <button onClick={handleClick}>요청 전송</button>;
+  return (
+    <>
+      <Header title={"데이터 요청 실습"}></Header>
+      <button onClick={handleClick}>요청 전송</button>
     </>
-  ) 
+  );
 };
 
 export default AxiosTest;
