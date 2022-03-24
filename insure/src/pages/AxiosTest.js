49e18206078773d12f4ad128c279a2b228557c37
707BIGBOSS\user <2001mist@naver.com>
2022-03-24 10:43:37 +0900

    엑시오스 테스트

--- insure/src/pages/AxiosTest.js
+++ insure/src/pages/AxiosTest.js
@@ -1,14 +1,22 @@
 import React from "react";
 import axios from "axios";
+import Header from "../components/Header";
 
 const AxiosTest = () => {
   const handleClick = () => {
-    axios.get("https://naver.com").then(function (response) {
+    axios.get(
+        "https://newsapi.org/v2/everything?q=tesla&from=2022-02-23&sortBy=publishedAt&apiKey=84c96b195b1e4e97b8cb2e5cd8977e9f"
+        ).then(function (response) {
       // handle success
       console.log(response);
     });
   };
-  return <button onClick={handleClick}>요청 전송</button>;
+  return(
+    <>  
+        <Header title={"데이터 요청 실습"}></Header>
+        <button onClick={handleClick}>요청 전송</button>;
+    </>
+  ) 
 };
 
 export default AxiosTest;
